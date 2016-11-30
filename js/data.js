5f8e0b121df8fec07e3aae0284ae7e8087089a9c
梵团子 <937351823@qq.com>
2016-11-30 16:00:57 +0800

    main主页的data

---
+++ js/data.js
@@ -0,0 +1,110 @@
+var main={
+	"id":1;
+	"data": [
+        {
+            "question_id": 1,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Emily",
+            "feed_source_txt": "赞了回答1",
+            "feed_source_img": "../images/avatar1.jpg",
+            "question": "什么是ETAG",
+            "answer_img":"../iamges/content1."
+            "answer_ctnt": "Etag是URL的Entity Tag，用于标示URL对象是否改变，区分不同语言和Session等等。具体内部含义是使服务器控制的，就像Cookie那样。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 2,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex",
+            "feed_source_txt": "回答了问题2",
+            "feed_source_img": "../images/avatar2.jpg",
+            "question": "请介绍下《上古卷轴》",
+            "answer_img":"../iamges/content2."
+            "answer_ctnt": "《上古卷轴》（The Elder Scrolls，简称 TES）是由Bethesda Softworks开发的一款角色扮演游戏（RPG）系列。系列首部作品是1994年3月在DOS平台上推出的《上古卷轴：竞技场》...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 3,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George",
+            "feed_source_txt": "赞了回答3",
+            "feed_source_img": "../images/avatar3.jpg",
+            "question": "威尼斯有哪些美丽的风景？",
+            "answer_img":"../iamges/content3."
+            "answer_ctnt": "威尼斯的风情总离不开“水”，蜿蜒的水巷，流动的清波，宛若默默含情的少女，眼底倾泻着温柔。其建筑、绘画、雕塑、歌剧等在世界有着极其重要的地位和影响。威尼斯有“因水而生，因水而美，因水而兴”的美誉，享有“水城”“水上都市”“百岛城”等美称。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 4,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Emily",
+            "feed_source_txt": "赞了回答4",
+            "feed_source_img": "../images/avatar1.jpg",
+            "question": "什么是ETAG？",
+            "answer_img":"../iamges/content1."
+            "answer_ctnt": "Etag是URL的Entity Tag，用于标示URL对象是否改变，区分不同语言和Session等等。具体内部含义是使服务器控制的，就像Cookie那样。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 5,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex",
+            "feed_source_txt": "回答了问题5",
+            "feed_source_img": "../images/avatar2.jpg",
+            "question": "请介绍下《上古卷轴》",
+            "answer_img":"../iamges/content2."
+            "answer_ctnt": "《上古卷轴》（The Elder Scrolls，简称 TES）是由Bethesda Softworks开发的一款角色扮演游戏（RPG）系列。系列首部作品是1994年3月在DOS平台上推出的《上古卷轴：竞技场》...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 6,
+            "answer_id": 61,
+            "feed_source_id": 25,
+            "feed_source_name": "George",
+            "feed_source_txt": "赞了回答6",
+            "feed_source_img": "../images/avatar3.jpg",
+            "question": "威尼斯有哪些美丽的风景？",
+            "answer_img":"../iamges/content3."
+            "answer_ctnt": "威尼斯的风情总离不开“水”，蜿蜒的水巷，流动的清波，宛若默默含情的少女，眼底倾泻着温柔。其建筑、绘画、雕塑、歌剧等在世界有着极其重要的地位和影响。威尼斯有“因水而生，因水而美，因水而兴”的美誉，享有“水城”“水上都市”“百岛城”等美称。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 7,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "Emily",
+            "feed_source_txt": "赞了回答7",
+            "feed_source_img": "../images/avatar1.jpg",
+            "question": "什么是ETAG？",
+            "answer_img":"../iamges/content1."
+            "answer_ctnt": "Etag是URL的Entity Tag，用于标示URL对象是否改变，区分不同语言和Session等等。具体内部含义是使服务器控制的，就像Cookie那样。",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 8,
+            "answer_id": 25,
+            "feed_source_id": 24,
+            "feed_source_name": "Alex",
+            "feed_source_txt": "回答了问题8",
+            "feed_source_img": "../images/avatar2.jpg",
+            "question": "请介绍下《上古卷轴》",
+            "answer_img":"../iamges/content2."
+            "answer_ctnt": "《上古卷轴》（The Elder Scrolls，简称 TES）是由Bethesda Softworks开发的一款角色扮演游戏（RPG）系列。系列首部作品是1994年3月在DOS平台上推出的《上古卷轴：竞技场》...",
+            "good_num": "112",
+            "comment_num": "18"
+        }
+
+    ]
+}
